8cf8d429b94c9cac054e4b5697e8aef592cb241f
mohammed <msalhibiade@cifpfbmoll.eu>
2023-01-27 19:33:28 +0100

    tablas final

--- ddl.sql
+++ ddl.sql
@@ -2,19 +2,19 @@ CREATE database bikes;
 
 CREATE TABLE bike(
     id int(50) primary key,
-    brand varchar(80),
-    model varchar(80),
-    category varchar(80),
-    tipo varchar(80),
-    orientation varchar(80),
-    contact_numbre int(9) unique,
-    rent_date date
+    brand varchar(80) not null,
+    model varchar(80) not null,
+    category varchar(80) not null,
+    tipo varchar(80) not null,
+    orientation varchar(80) not null,
+    contact_numbre int(9) not null unique,
+    rent_date date not null
 ); 
 
 CREATE TABLE orientation(
     id int(50),
-    orientation_id varchar(80) ,
-    size varchar(80),
-    location varchar(80),
+    orientation_id varchar(80) not null,
+    size varchar(80) not null,
+    location varchar(80) not null,
     foreign key (id) references bike (id)
 );
